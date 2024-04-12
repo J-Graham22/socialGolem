2ac3ac857910bc5f3bd757761f3fda61ca132c0c
Justin <justin.m.graham2@gmail.com>
2024-04-12 10:30:52 -0400

    refined and organized

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import tweepy
 import time
 import webbrowser
 import private
 
-auth = tweepy.OAuthHandler(private.TWITTER_APP_KEY, private.TWITTER_APP_SECRET, private.CALLBACK_URI)
-redirect_url = auth.get_authorization_url()
-webbrowser.open(redirect_url)
+def main():
+    api = authenticate_with_tweepy()
 
-user_pin_input = input("What's the pin value?")
+    while True:
+        like_and_retweet(api)
+        time.sleep(10)
 
-auth.set_access_token(private.TWITTER_KEY, private.TWITTER_SECRET)
-api = tweepy.API(auth)
+def authenticate_with_tweepy():
+    auth = tweepy.OAuthHandler(private.TWITTER_APP_KEY, private.TWITTER_APP_SECRET, private.CALLBACK_URI)
+    redirect_url = auth.get_authorization_url()
+    webbrowser.open(redirect_url)
 
-# current users to check for in the loop
-nc_sam = "IamNCSam"
-punx = "gg_punx"
+    auth.set_access_token(private.TWITTER_KEY, private.TWITTER_SECRET)
+    api = tweepy.API(auth)
+    return api
 
-url_list = []
-url_list.append(nc_sam)
-url_list.append(punx)
-
-def like_and_retweet():
-    for user in url_list:
+def like_and_retweet(api):
+    username_list = private.USERNAME_LIST
+    
+    for user in username_list:
         for tweet in tweepy.Cursor(api.home_timeline, id=user, include_entities=True).items(5):
             id = tweet.id
-            if 't.co' in tweet.text:
-                if not tweet.retweeted:
-                    api.retweet(id)
-                if not tweet.favorited:
-                    api.create_favorite(id)
+            if not tweet.retweeted:
+                api.retweet(id)
+            if not tweet.favorited:
+                api.create_favorite(id)
 
-while True:
-    like_and_retweet()
-    time.sleep(10)
+if __name__ == '__main__':
+    main()
---
+++ private.py
@@ -0,0 +1,8 @@
+TWITTER_APP_KEY = ""
+TWITTER_APP_SECRET = ""
+CALLBACK_URI = ""
+
+TWITTER_KEY = ""
+TWITTER_SECRET = ""
+
+USERNAME_LIST = [] #should be filled with the usernames of users you would like to automatically like and retweet
